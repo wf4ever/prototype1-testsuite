1e69762de2cd7dd9b4dcad2877356cb08075f552
Graham Klyne <graham@angua.(none)>
2011-03-22 11:02:28 +0000

    git ignore generated files and ediutor backups

--- TestCreateRO.sh
+++ TestCreateRO.sh
@@ -42,7 +42,7 @@ prefix rdf:     <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
 prefix rdfs:    <http://www.w3.org/2000/01/rdf-schema#>
 prefix dcterms: <http://purl.org/dc/terms/>
 prefix oxds:    <http://vocab.ox.ac.uk/dataset/schema#>
-ASK { ?s dcterms:identifier "TestRO1zz" }
+ASK { ?s dcterms:identifier "TestRO1" }
 END
 
 cp TestRO1-manifest.rdf ${DropBoxDir}/TestRO1/manifest.rdf

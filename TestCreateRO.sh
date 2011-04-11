651221839dc577c83dd85f20869b4a3d8ceacc9d
Stian Soiland-Reyes <stian@soiland-reyes.com>
2011-04-11 15:40:36 +0100

    Increase timeout. Auto-generate RO name (to allow multiple invocations). export ARQROOT

--- TestCreateRO.sh
+++ TestCreateRO.sh
@@ -11,18 +11,30 @@
 #   ${DropBoxDir}
 #       connects via DropBox to a storage area that is being monitored by
 #       the RO SRS DropBox interface shim
-#
+#   ARQ-2.8.7
+#       for SparQL queries over manifest
+
+export ARQROOT=$(pwd)/ARQ-*/
+
+
+# Ensure RO name is unique
+if (type -P uuidgen 2>/dev/null >/dev/null); then 
+    RO="TestRO$(uuidgen)"
+else 
+    RO="TestRO$$"
+fi
+
 
 echo "TestCreateRO - START: Test create RO"
 
 source TestConfig.sh
 
-echo " - Create local RO instance"
+echo " - Create local RO instance ${RO}"
 
-mkdir -p ${DropBoxDir}/TestRO1
+mkdir -p ${DropBoxDir}/${RO}
 
 # Expected initial manifest @@TODO is this really required?
-cat >TestRO1-manifest.rdf - <<END
+cat >${RO}-manifest.rdf - <<END
 <?xml version="1.0" encoding="utf-8"?>
 <rdf:RDF
   xmlns:dcterms="http://purl.org/dc/terms/"
@@ -30,36 +42,36 @@ cat >TestRO1-manifest.rdf - <<END
   xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
 >
   <oxds:Grouping>
-    <dcterms:identifier>TestRO1</dcterms:identifier>
-    <dcterms:description>Description of TestRO1</dcterms:description>
-    <dcterms:title>Test research object TestRO1</dcterms:title>
+    <dcterms:identifier>${RO}</dcterms:identifier>
+    <dcterms:description>Description of ${RO}</dcterms:description>
+    <dcterms:title>Test research object ${RO}</dcterms:title>
     <dcterms:creator>prototype1-testsuite</dcterms:creator>
   </oxds:Grouping>
 </rdf:RDF>
 END
 
-cat >TestRO1-manifest-query.sparql - <<END
+cat >${RO}-manifest-query.sparql - <<END
 prefix rdf:     <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
 prefix rdfs:    <http://www.w3.org/2000/01/rdf-schema#>
 prefix dcterms: <http://purl.org/dc/terms/>
 prefix oxds:    <http://vocab.ox.ac.uk/dataset/schema#>
-ASK { ?s dcterms:identifier "TestRO1" }
+ASK { ?s dcterms:identifier "${RO}" }
 END
 
 # Don't do this:  RObox should create the manifest
-# cp TestRO1-manifest.rdf ${DropBoxDir}/TestRO1/manifest.rdf
+# cp ${RO}-manifest.rdf ${DropBoxDir}/${RO}/manifest.rdf
 
 echo " - Waiting for SRS to create RO and metadata"
-sleep 5
+sleep 180
 
 echo " - Look for manifest with SRS metadata"
 
-if [ ! -e ${DropBoxDir}/TestRO1/manifest.rdf ] ; then
+if [ ! -e ${DropBoxDir}/${RO}/manifest.rdf ] ; then
     echo "TestCreateRO - FAIL: manifest.rdf not created"
     exit 1
 else
     # test new manifest contents
-    if ! arq_query_ask ${DropBoxDir}/TestRO1/manifest.rdf TestRO1-manifest-query.sparql ; then
+    if ! arq_query_ask ${DropBoxDir}/${RO}/manifest.rdf ${RO}-manifest-query.sparql ; then
         echo "TestCreateRO - FAIL: manifest wrong id"
         exit 1
     fi
